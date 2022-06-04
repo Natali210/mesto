6a72a9953ba75cfad871e0833900eb5a794c6555
Natalia Sarycheva <natali.ttm@yandex.ru>
2022-06-04 16:16:47 +0300

    ПР4 правки после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,30 @@
 const editButton = document.querySelector('.profile__edit')
 const popup = document.querySelector('.popup')
 const closePopupButton = document.querySelector('.popup__close')
-const nameField = document.querySelector('.popup__name-item')
+const nameField = document.querySelector('.popup__form-item_field_name')
 const profileTitle = document.querySelector('.profile__title')
-const aboutField = document.querySelector('.popup__about-item') 
+const aboutField = document.querySelector('.popup__form-item_field_about') 
 const profileSubtitle = document.querySelector('.profile__subtitle')
-const formElement = document.querySelector('.popup__edit-form')
+const formElement = document.querySelector('.popup__form')
 
+//Функция для открытия попапа через кнопку "Редактировать"
 function openPopup(popupElement) {
   popupElement.classList.add('popup_opened')
   nameField.value = profileTitle.textContent;
   aboutField.value = profileSubtitle.textContent;
 }
 
+//Функция для закрытия попапа
 function closePopup(popupElement) {
   popupElement.classList.remove('popup_opened')
+}
+
+//Функция для сохранения заполненной формы
+function submitPopup(event) {
+  event.preventDefault()
+  profileTitle.textContent = nameField.value;
+  profileSubtitle.textContent = aboutField.value;
+  closePopup(popup)
 }
 
 editButton.addEventListener('click', function() {
@@ -25,9 +35,4 @@ closePopupButton.addEventListener('click', function() {
   closePopup(popup)
 })
 
-formElement.addEventListener('submit', function(event) {
-  event.preventDefault()
-  closePopup(popup)
-  profileTitle.textContent = nameField.value;
-  profileSubtitle.textContent = aboutField.value;
-})
+formElement.addEventListener('submit', submitPopup);

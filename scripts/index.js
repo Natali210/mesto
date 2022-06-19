ac0e3011f074aa56f346ae9e12f5c2fe9a7df904
Natalia Sarycheva <natali.ttm@yandex.ru>
2022-06-19 22:35:05 +0300

    ПР5 Оптимизация наименований и другие правки после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 const placeField = document.querySelector('.popup__form-item_field_place');
 const linkField = document.querySelector('.popup__form-item_field_image-link');
 const formElementCard = document.querySelector('.popup__form_new-card');
-const editButton = document.querySelector('.profile__edit');
+const profileEditButton = document.querySelector('.profile__edit');
 const popupProfile = document.querySelector('.popup_profile');
 const closePopupProfile = document.querySelector('.popup__close_profile');
 const closePopupAddCard = document.querySelector('.popup__close_add-card');
@@ -10,7 +10,7 @@ const nameField = document.querySelector('.popup__form-item_field_name');
 const profileTitle = document.querySelector('.profile__title');
 const aboutField = document.querySelector('.popup__form-item_field_about');
 const profileSubtitle = document.querySelector('.profile__subtitle');
-const formElement = document.querySelector('.popup__form');
+const formElementProfile = document.querySelector('.popup__form_profile');
 const popupAddCard = document.querySelector('.popup_add-card');
 const addCardButton = document.querySelector('.profile__add');
 const popupImage = document.querySelector('.popup_open-image');
@@ -19,46 +19,46 @@ const imageFieldElement = document.querySelector('.popup__image-element');
 
 //Функция для открытия попапа "Редактировать профиль" 
 function openPopup(popupElement) {
-  popupElement.classList.add('popup_opened')
+  popupElement.classList.add('popup_opened');
   nameField.value = profileTitle.textContent;
   aboutField.value = profileSubtitle.textContent;
-}
+};
 
 //Функция для закрытия попапа
 function closePopup(popupElement) {
-  popupElement.classList.remove('popup_opened')
-}
+  popupElement.classList.remove('popup_opened');
+};
 
 //Функция для сохранения заполненной формы "Редактировать профиль"
-function submitPopup(evt) {
-  evt.preventDefault()
+function handleProfilePopup(evt) {
+  evt.preventDefault();
   profileTitle.textContent = nameField.value;
   profileSubtitle.textContent = aboutField.value;
-  closePopup(popupProfile)
-}
+  closePopup(popupProfile);
+};
 
 //Действия с формой "Редактировать профиль"
-editButton.addEventListener('click', function () {
-  openPopup(popupProfile)
-})
+profileEditButton.addEventListener('click', function () {
+  openPopup(popupProfile);
+});
 
 closePopupProfile.addEventListener('click', function () {
-  closePopup(popupProfile)
+  closePopup(popupProfile);
 })
 
 closePopupImage.addEventListener('click', function () {
-  closePopup(popupImage)
+  closePopup(popupImage);
 })
 
-formElement.addEventListener('submit', submitPopup);
+formElementProfile.addEventListener('submit', handleProfilePopup);
 
 //Действия с формой "Добавить карточку места"
 addCardButton.addEventListener('click', function () {
-  openPopup(popupAddCard)
+  openPopup(popupAddCard);
 })
 
 closePopupAddCard.addEventListener('click', function () {
-  closePopup(popupAddCard)
+  closePopup(popupAddCard);
 })
 
 const cardTemplate = document.querySelector('#element-template').content;
@@ -69,6 +69,7 @@ const createCard = (element) => {
 
   cardElement.querySelector('.element__title').textContent = element.name;
   cardImage.src = element.link;
+  cardImage.alt = element.name;
 
   cardElement.querySelector('.element__like').addEventListener('click', (evt) => {
     evt.target.classList.toggle('element__like_active');
@@ -79,17 +80,17 @@ const createCard = (element) => {
   });
 
   cardImage.addEventListener('click', () => {
+    const openImageCardPopup = (element) => {
+      imageFieldElement.src = element.link;
+      imageFieldElement.alt = element.name;
+      captionFieldElement.textContent = element.name;
+    };
+
     openPopup(popupImage);
     openImageCardPopup(element);
   });
 
   return cardElement;
-};
-
-const openImageCardPopup = (element) => {
-  imageFieldElement.src = element.link;
-  imageFieldElement.alt = element.name;
-  captionFieldElement.textContent = element.name;
 };
 
 const addCard = (element) => {
@@ -101,7 +102,7 @@ const addCard = (element) => {
 
 //Функция для сохранения заполненной формы "Новое место"
 const handlePopupCard = (evt) => {
-  evt.preventDefault()
+  evt.preventDefault();
 
   const card = {
     name: placeField.value,

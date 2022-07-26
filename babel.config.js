75da2f90b4ed5bd1a5d282a14464357f645e7530
Natalia Sarycheva <natali.ttm@yandex.ru>
2022-07-26 16:09:37 +0300

    ПР8: добавление babel-core

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,15 @@
-module.exports = {
-  presets: [
-    [
-      "@babel/preset-env",
-      {
-        "corejs": "3.24.0",
-        'useBuiltIns': "entry"
-      }  
-    ]
-  ]
-}
+const presets = [
+  ['@babel/preset-env', {
+    targets: {
+      edge: '17',
+      ie: '11',
+      firefox: '50',
+      chrome: '64',
+      safari: '11.1'
+    },
+    "corejs": "3.24.0",
+    useBuiltIns: "entry"
+  }]
+];
+
+module.exports = { presets };
